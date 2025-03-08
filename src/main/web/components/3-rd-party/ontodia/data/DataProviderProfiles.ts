21a84a3da96474fb1994b4f3e01e907dc0d84105
Alexey Morozov <alexey.morozov@live.ru>
2025-03-08 19:10:20 +0300

    Replace vendored `ontodia` package by `@reactodia/workspace` fork: * Update usages according to the breaking changes; * Copy missing bits from legacy Ontodia implementation (`GraphBuilder`, builtin toolbar wrapper); * Partially fix styles.

--- src/main/web/components/3-rd-party/ontodia/data/DataProviderProfiles.ts
+++ src/main/web/components/3-rd-party/ontodia/data/DataProviderProfiles.ts
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { SparqlDataProviderSettings } from 'ontodia';
+import { SparqlDataProviderSettings } from '@reactodia/workspace';
 
 export const WikidataSettings: SparqlDataProviderSettings = {
   linkConfigurations: [],
